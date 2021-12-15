282f95f59d8d16cd86773ae685630f29a5fb855c
Uroš Pantelić <pantelicu@triglavzdravje.si>
2021-12-15 10:30:09 +0100

    new project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
